659c832543ac298d0f06b4ed2f49e3939bcf315a
dustinmiler <dustin.miller@healthalliance.org>
2016-02-18 18:34:38 -0600

    Adding validation

--- media/app.js
+++ media/app.js
@@ -1,15 +1,46 @@
 $(document).ready(function() {
 	SubsidyCalc.init({
-		//zipcode data will come from whatever will be holding the location data
-		age:'[name=age]',
+		//some of this data will probably(?) be coming from other data sources
+        //we can populate the input fields automatically so we don't have to change the
+        //module around or change the module to grab that data, although that does make
+        //the module dependent on whatever is supplying that data.
 		zip:'[name=zip]',
 		size:'[name=size]',
 		income:'[name=income]',
 		insured:'[name=insured]',
-		insuredAge:'[name=insuredAge]',
+		insuredAgeWrapper:'#insured-age-inputs',
+        subsidyResultsWrapper:'#subsidy-content',
+        subsidyResultsContent: '#subsidy-details',
+        submitButton: '#calculate',
 		form:'#subsidy-calc'	
 	});
-    $('#subsidy-calc').validate();
+
+    $('#subsidy-calc').validate({
+        rules: {
+                zip: {
+                    required: true,
+                    minlength: 5,
+                    maxlength: 5,
+                    digits: true
+                },
+                size: {
+                    required: true,
+                    minlength: 1,
+                    maxlength: 2,
+                    digits: true
+                },
+                income: {
+                    required: true,
+                    digits: true
+                },
+                insured: {
+                    required: true,
+                    minlength: 1,
+                    maxlength: 2,
+                    digits: true
+                }
+            },    
+    });
 });
 
 var SubsidyCalc = (function(){
@@ -19,11 +50,11 @@ var SubsidyCalc = (function(){
     }
 
 	function handleInsured(evt) {
-		$('#insured-age-inputs').html("");
+		$insuredAgeWrapper.html("");
 		for (var i = 1; i <= $insured.val(); i=i+1) {
 			var template = Handlebars.compile($('#age-insured').html()),
 				context = {number:i}
-			$('#insured-age-inputs').append(template(context));
+			$insuredAgeWrapper.append(template(context));
         }
     }
 
@@ -32,9 +63,11 @@ var SubsidyCalc = (function(){
     		fplHouseSize = subsidyData.fpl[0].increment * multiplier + subsidyData.fpl[0].base,
     		found = false,
             planCost = 0,
-            template = Handlebars.compile($('#subsidy-details').html()),
+            template = Handlebars.compile($subsidyResultsContent.html()),
             context,
             contributionAmount;
+
+        $subsidyResultsWrapper.html();
 
     	subsidyData.fpl.map(function(row, index){
     		var upperValue = row.increment * multiplier + row.base,
@@ -52,18 +85,24 @@ var SubsidyCalc = (function(){
 
     			contributionAmount = $houseIncome.val() * (contributionPercent / 100);
 
-    			$('#insured-age-inputs :input').each(function(index,element) {  
+                $insuredAgeInputs = $($insuredAgeInputs);
+    			$insuredAgeInputs.each(function(index,element) {  
+                    console.log(element);
                     var property = $(element).val() === 20 ? "_0_20_child_dependents" : "_" + $(element).val();
                     planCost += parseInt(slcspData[property]);
 				});
+
                 context = {
-                    monthInsureCost:planCost - (planCost - (contributionAmount / 12)),
-                    monthCost:planCost,    
-                    yearCost:planCost*12,
-                    monthTax:planCost - (contributionAmount / 12),
-                    yearTax:(planCost * 12) - contributionAmount,
+                    monthInsureCost:(planCost - (planCost - (contributionAmount / 12))).toFixed(2),
+                    monthCost:planCost.toFixed(2),    
+                    yearCost:(planCost*12).toFixed(2),
+                    monthTax:(planCost - (contributionAmount / 12)).toFixed(2),
+                    yearTax:((planCost * 12) - contributionAmount).toFixed(2),
                 }
-                $('#subsidy-content').append(template(context));
+
+                $subsidyResultsWrapper.html("");
+
+                $subsidyResultsWrapper.append(template(context));
     		}	
     	});      
     }
@@ -86,21 +125,35 @@ var SubsidyCalc = (function(){
         });      
     }
 
+    function handleFormValidation(evt) {
+        $formErrors = $($formErrors);
+        if($formErrors.length > 0) {
+            $submitButton.prop("disabled", true);    
+        } else {
+            $submitButton.prop("disabled", false); 
+        }
+           
+    }
+
 	function init(opts) {
         $houseSize = $(opts.size);
         $houseIncome = $(opts.income);
         $zip = $(opts.zip);
-        $age = $(opts.age);
         $insured = $(opts.insured);
-        $insuredAge = $(opts.insuredAge);
+        $insuredAgeWrapper = $(opts.insuredAgeWrapper);
+        $insuredAgeInputs = opts.insuredAgeWrapper + ' :input';
+        $subsidyResultsContent = $(opts.subsidyResultsContent);
+        $subsidyResultsWrapper = $(opts.subsidyResultsWrapper);
+        $submitButton = $(opts.submitButton);
+        $formErrors = opts.form + ' .error';
         $form = $(opts.form);
 
         //listen for changes and add the number of needed inputs
         $insured.bind('blur',handleInsured.bind(this));
         //form submission
         $form.bind('submit',handleCalculate.bind(this));
-        console.log($form.validate());
 
+        //Events to validate the form fields along with data retrieval from API on the zip field
         $zip.bind('blur',handleRateRetrieval.bind(this));
     }
 
@@ -111,12 +164,14 @@ var SubsidyCalc = (function(){
         //DOM References
         $houseSize,
         $houseIncome,
-        //This reference will need to be changed in production to whatever is holding the location data
         $zip,
-        $age,
         $insured,
-        $insuredAge,
-        $calculate,
+        $insuredAgeWrapper,
+        $insuredAgeInputs,
+        $subsidyResultsWrapper,
+        $subsidyResultsContent,
+        $submitButton,
+        $form,
 
         publicAPI = {
             init: init,
@@ -127,15 +182,7 @@ var SubsidyCalc = (function(){
     return publicAPI;	
 })();
 
-/*SubsidyCalc.loadJSON(
-	[{"lowFpl": 0, "minEarn":100, "maxEarn":133, "minContribution":2.03, "maxContribution":2.03, "base":11770, "increment":4160},
-	{"lowFpl": 100, "minEarn":133, "maxEarn":150, "minContribution":3.05, "maxContribution":4.07, "base":15654, "increment":5533},
-	{"lowFpl": 133, "minEarn":150, "maxEarn":200, "minContribution":4.07, "maxContribution":6.41, "base":17655, "increment":6240},
-	{"lowFpl": 150, "minEarn":200, "maxEarn":250, "minContribution":6.41, "maxContribution":8.18, "base":23540, "increment":8320},
-	{"lowFpl": 200, "minEarn":250, "maxEarn":300, "minContribution":8.18, "maxContribution":9.66, "base":35310, "increment":12480},
-	{"lowFpl": 250, "minEarn":300, "maxEarn":400, "minContribution":9.66, "maxContribution":9.66, "base":47080, "increment":16640}]
-);*/
-
+//base is not really the base. bad naming convention by me. I should change it to first or one or something similar...
 SubsidyCalc.loadJSON(
 	{"fpl":[
 		{"percent":100, "base":11770, "increment":4160},
